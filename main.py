27f152c1d1adf5cc9dac88d0a3a1b1ea32e5c3ea
Olga-Pro <oip_@inbox.ru>
2024-05-08 18:53:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
-import pprint # библиотека для печати словарей
+import pprint  # библиотека для печати словарей
 
 # Задание 1 (см.README.md)
 url = " https://api.github.com"
-my_params = {"q": 'html'} # параметры запроса в формате словаря
+my_params = {"q": 'html'}  # параметры запроса в формате словаря
 
 response = requests.get(url, params=my_params)
 print("Задание 1")
@@ -15,7 +15,7 @@ pprint.pprint(result1)
 
 # Задание 2 (см.README.md)
 url = "https://jsonplaceholder.typicode.com/posts"
-my_params = {"userId": '1'} # параметры запроса в формате словаря
+my_params = {"userId": '1'}  # параметры запроса в формате словаря
 
 response = requests.get(url, params=my_params)
 result2 = response.json()
@@ -26,8 +26,8 @@ pprint.pprint(result2)
 # Задание 3 (см.README.md)
 url = "https://jsonplaceholder.typicode.com/posts"
 my_data = {'title': 'foo',
-             'body': 'bar',
-             'userId': 1} #параметры запроса в формате словаря
+           'body': 'bar',
+           'userId': 1}  # параметры запроса в формате словаря
 
 response = requests.post(url, data=my_data)
 print("Задание 3")
@@ -37,4 +37,3 @@ result3 = response.json()
 print(f"Ответ post-запроса {result3}")
 print("Еще вариант вывода результата запроса в формате JSON:")
 pprint.pprint(result3)
-
